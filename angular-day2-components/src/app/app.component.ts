efb2db92a073782a873b9e6defe5ade59c2665a2
sripuram ramya <sripuramramya275@gmail.com>
2025-06-11 05:04:33 -0400

    header component

---
+++ angular-day2-components/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './header/header.component'
+@Component({
+  selector: 'app-root',
+  imports: [HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular-day2-components';
+}
